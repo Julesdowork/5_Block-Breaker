124c93c18926bd1de8039821db1ae6a75e7dd879
Julian McNeill <julesdowork@gmail.com>
2019-03-18 22:20:02 -0400

    Colliders, Collision, & Triggers

---
+++ Super Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    [SerializeField] string loseScene;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        SceneManager.LoadScene(loseScene);
+    }
+}
