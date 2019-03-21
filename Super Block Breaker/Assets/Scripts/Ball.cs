a4ad91231d15a9269e60e817713c8a23243aa0d3
Julian McNeill <julesdowork@gmail.com>
2019-03-20 21:05:08 -0400

    Play Random Sound From Array

--- Super Block Breaker/Assets/Scripts/Ball.cs
+++ Super Block Breaker/Assets/Scripts/Ball.cs
@@ -8,15 +8,20 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
 
     // State
     Vector2 paddleToBallVector;
     bool hasStarted = false;
 
+    // Cached component references
+    AudioSource myAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -32,7 +37,10 @@ public class Ball : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (hasStarted)
-            GetComponent<AudioSource>().Play();
+        {
+            AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
+            myAudioSource.PlayOneShot(clip);
+        }
     }
 
     private void LockToPaddle()
