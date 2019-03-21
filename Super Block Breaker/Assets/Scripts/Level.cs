211c6f2273db6ffe10bf28b389aa0681f8eaff68
Julian McNeill <julesdowork@gmail.com>
2019-03-20 22:17:26 -0400

    Win The Level & Load

--- Super Block Breaker/Assets/Scripts/Level.cs
+++ Super Block Breaker/Assets/Scripts/Level.cs
@@ -6,8 +6,25 @@ public class Level : MonoBehaviour
 {
     [SerializeField] int breakableBlocks;   // serialized for debugging purposes
 
+    // Cached reference
+    SceneLoader sceneLoader;
+
+    void Start()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneLoader.LoadNextScene();
+        }
     }
 }
