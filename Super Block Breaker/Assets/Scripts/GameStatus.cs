dcf252c41f3997022663673bd2ca5ece684ac802
Julian McNeill <julesdowork@gmail.com>
2019-03-20 22:50:25 -0400

    Display Player Score

--- Super Block Breaker/Assets/Scripts/GameStatus.cs
+++ Super Block Breaker/Assets/Scripts/GameStatus.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour
 {
     // Configuration parameters
     [Range(0.1f, 10f)] [SerializeField] float gameSpeed = 1f;
     [SerializeField] int pointsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     // State
     [SerializeField] int currentScore = 0;
+
+    void Start()
+    {
+        DisplayScore();
+    }
 
     // Update is called once per frame
     void Update()
@@ -20,5 +27,11 @@ public class GameStatus : MonoBehaviour
     public void AddToScore()
     {
         currentScore += pointsPerBlockDestroyed;
+        DisplayScore();
+    }
+
+    void DisplayScore()
+    {
+        scoreText.text = currentScore.ToString();
     }
 }
