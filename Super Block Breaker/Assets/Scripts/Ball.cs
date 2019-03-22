f0ccd168ab0e11415f7b2983b0b2b00d6a4b0fde
Julian McNeill <julesdowork@gmail.com>
2019-03-21 23:05:47 -0400

    Prevent Boring Ball Loops

--- Super Block Breaker/Assets/Scripts/Ball.cs
+++ Super Block Breaker/Assets/Scripts/Ball.cs
@@ -9,6 +9,7 @@ public class Ball : MonoBehaviour
     [SerializeField] float xPush = 2f;
     [SerializeField] float yPush = 15f;
     [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
 
     // State
     Vector2 paddleToBallVector;
@@ -16,12 +17,14 @@ public class Ball : MonoBehaviour
 
     // Cached component references
     AudioSource myAudioSource;
+    Rigidbody2D myRigidbody2D;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBallVector = transform.position - paddle1.transform.position;
         myAudioSource = GetComponent<AudioSource>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -36,10 +39,13 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 velocityTweak = new Vector2(
+            Random.Range(0, randomFactor), Random.Range(0, randomFactor));
         if (hasStarted)
         {
             AudioClip clip = ballSounds[Random.Range(0, ballSounds.Length)];
             myAudioSource.PlayOneShot(clip);
+            myRigidbody2D.velocity += velocityTweak;
         }
     }
 
@@ -54,7 +60,7 @@ public class Ball : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             hasStarted = true;
-            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush, yPush);
+            myRigidbody2D.velocity = new Vector2(xPush, yPush);
         }
     }
 }
