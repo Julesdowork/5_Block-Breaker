f636fcbd553c47840bad3a4d7cc010d519d78509
Julian McNeill <julesdowork@gmail.com>
2019-03-21 21:55:56 -0400

    Instantiate GameObject

--- Super Block Breaker/Assets/Scripts/Block.cs
+++ Super Block Breaker/Assets/Scripts/Block.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
 
     // Cached reference
     Level level;
@@ -22,9 +23,21 @@ public class Block : MonoBehaviour
 
     private void DestroyBlock()
     {
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-        FindObjectOfType<GameSession>().AddToScore();
+        PlayDestroySFX();
+        TriggerSparkles();
         Destroy(gameObject);
         level.BlockDestroyed();
+    }
+
+    private void TriggerSparkles()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 2f);
+    }
+
+    private void PlayDestroySFX()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        FindObjectOfType<GameSession>().AddToScore();
     }
 }
