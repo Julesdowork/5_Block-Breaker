ea22a88f32831bd52b488a9c0076293b35cbfe11
Julian McNeill <julesdowork@gmail.com>
2019-05-21 21:48:27 -0400

    BQ - Create SceneLoader

---
+++ Breakout Quest/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] string startScene = "1_Start";
+    [SerializeField] string gameOverScene = "4_Game Over";
+
+    public void LoadNextScene()
+    {
+        int currentLevel = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentLevel + 1);
+    }
+
+    public void LoadGameOverScene()
+    {
+        SceneManager.LoadScene(gameOverScene);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(startScene);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game");
+        Application.Quit();
+    }
+}
