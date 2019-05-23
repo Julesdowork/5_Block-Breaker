5db833f6d290d92a564be935f58debd4a60b8ffa
Julian McNeill <julesdowork@gmail.com>
2019-05-22 21:49:09 -0400

    BQ - Winning and losing levels

---
+++ Breakout Quest/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour
+{
+    SceneLoader sceneLoader;
+
+    void Awake()
+    {
+        sceneLoader = FindObjectOfType<SceneLoader>();
+    }
+
+    void OnTriggerEnter2D()
+    {
+        sceneLoader.LoadGameOverScene();
+    }
+}
