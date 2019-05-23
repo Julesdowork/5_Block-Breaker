15dc1002ccfcf3478f1309381e84a87249dfeb8a
Julian McNeill <julesdowork@gmail.com>
2019-05-22 21:40:25 -0400

    Create Block and Level classes

--- Breakout Quest/Assets/Scripts/Block.cs
+++ Breakout Quest/Assets/Scripts/Block.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] Sprite[] hitSprites;
+    int maxHits;
+
+    Level level;
+
+    [SerializeField] int timesHit;  // TODO deserialize this
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        CountBreakableBlocks();
+        maxHits = hitSprites.Length + 1;
     }
 
     // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (gameObject.CompareTag("Breakable"))
+        {
+            HandleHit();
+        }
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (gameObject.CompareTag("Breakable"))
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        Destroy(gameObject);
+        level.BlockDestroyed();
     }
 }
